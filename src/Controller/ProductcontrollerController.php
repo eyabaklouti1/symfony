5d74b6f8a44da1db3bcf0ce6a316a5c78b687de9
eyabaklouti1 <bakloutie2@gmail.com>
2025-07-19 04:38:10 +0200

    edit+delete done

--- src/Controller/ProductcontrollerController.php
+++ src/Controller/ProductcontrollerController.php
@@ -7,7 +7,7 @@ use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 
 
@@ -31,19 +31,19 @@ final class ProductcontrollerController extends AbstractController
         ]);
     }
 
-    #[Route('/create-product', name: 'create-product')] 
+    #[Route('/create-product', name: 'create_product')] 
     public function createproduct(Request $request)
     {
         $product = new Product();   //the object of the entity
         $form = $this->createForm(ProductType::class, $product); //générer le formulaire
         $form->handleRequest($request); //remplir le formulaire avec les données soumises
         if($form->isSubmitted() && $form->isValid()){
-            $this->em->persist($product);
-            $this->em->flush();
+            $this->em->persist($product); //pour enregistrer une entité
+            $this->em->flush(); //flush() : pour exécuter les requêtes SQL
 
 
             $this->addFlash('message','Inserted Successfully.');
-
+            return $this->redirectToRoute('app_productcontroller');
         }
         return $this->render('productcontroller/product.html.twig', [
             'form' => $form->createView(), // product.html.twig contient le code HTML du formulaire.
@@ -51,5 +51,41 @@ final class ProductcontrollerController extends AbstractController
 
         ]);
     }
+    #[Route('/edit-product/{id}', name:'edit_product')]
+    public function editproduct(Request $request , int $id): Response
+    {
+        $product = $this->em->getRepository(Product::class)->find($id);
+        if (!$product) {
+             throw $this->createNotFoundException('Product not found.');
+        }
+        $form = $this->createForm(ProductType::class , $product);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isvalid()){
+            $this->em->persist($product);
+            $this->em->flush();
+
+
+            $this->addFlash('message','Updated Successfully.');
+            return $this->redirectToRoute('app_productcontroller');
+        }
+         return $this->render('productcontroller/product.html.twig', [
+            'form' => $form->createView(),
+        ]);
     
+    }
+    #[Route('/delete-product/{id}' , name:'delete_product')]
+    public function deleteproduct($id): Response
+    {
+        $product = $this->em->getRepository(Product::class)->find($id);
+        if (!$product){
+            throw $this->createNotFoundException('Product not found.');
+        }
+        $this->em->remove($product);
+        $this->em->flush();
+
+        $this->addFlash('message','Deleted Syccessfully.');
+        return $this->redirectRoute('app_productcontroller');
+
+
+    }
 }
