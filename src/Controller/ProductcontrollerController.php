f57bd1068121398f17ab0044f6220ddc569d7cf9
eyabaklouti1 <bakloutie2@gmail.com>
2025-07-20 01:56:56 +0200

    crud product entity done

--- src/Controller/ProductcontrollerController.php
+++ src/Controller/ProductcontrollerController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use App\Form\ProductType;
 use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
@@ -21,13 +21,12 @@ final class ProductcontrollerController extends AbstractController
     }  
 
     #[Route('/productcontroller', name: 'app_productcontroller')]
-    public function index(): JsonResponse{
+    public function index(): Response{
 
          $products = $this->em->getRepository(Product::class)->findAll();
     
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProductcontrollerController.php',
+        return $this->render('productcontroller/index.html.twig',[
+                'products' => $products
         ]);
     }
 
@@ -45,12 +44,24 @@ final class ProductcontrollerController extends AbstractController
             $this->addFlash('message','Inserted Successfully.');
             return $this->redirectToRoute('app_productcontroller');
         }
-        return $this->render('productcontroller/product.html.twig', [
+        return $this->render('productcontroller/create.html.twig', [
             'form' => $form->createView(), // product.html.twig contient le code HTML du formulaire.
 
 
         ]);
     }
+    #[Route('/read-product/{id}', name:'read_product')]
+    public function readproduct(int $id): Response
+    {
+        $product = $this->em->getRepository(Product::class)->find($id);
+        if (!$product){
+            throw $this->createNotFoundException('Product not found.');
+        }
+        return $this->render('productcontroller/read.html.twig',[
+            'product' => $product,
+        ]);
+    }
+    
     #[Route('/edit-product/{id}', name:'edit_product')]
     public function editproduct(Request $request , int $id): Response
     {
@@ -68,7 +79,7 @@ final class ProductcontrollerController extends AbstractController
             $this->addFlash('message','Updated Successfully.');
             return $this->redirectToRoute('app_productcontroller');
         }
-         return $this->render('productcontroller/product.html.twig', [
+         return $this->render('productcontroller/edit.html.twig', [
             'form' => $form->createView(),
         ]);
     
@@ -78,13 +89,14 @@ final class ProductcontrollerController extends AbstractController
     {
         $product = $this->em->getRepository(Product::class)->find($id);
         if (!$product){
-            throw $this->createNotFoundException('Product not found.');
+           $this->addFlash('error', 'Product not found.');
+        return $this->redirectToRoute('app_productcontroller');
         }
         $this->em->remove($product);
         $this->em->flush();
 
         $this->addFlash('message','Deleted Syccessfully.');
-        return $this->redirectRoute('app_productcontroller');
+        return $this->redirectToRoute('app_productcontroller');
 
 
     }
