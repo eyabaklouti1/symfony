8256e89db3ad287a90aca517298288f4edd0e689
eyabaklouti1 <bakloutie2@gmail.com>
2026-01-21 01:04:36 +0100

    Ajout d’une page d’accueil initiale

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        // Récupère tous les produits (avec leurs catégories et sous-catégories)
+        $products = $productRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'products' => $products,
+        ]);
     }
 }
